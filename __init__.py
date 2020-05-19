09e5b90e1b78432537cc5ff059e44f8a40718eea
GayBroYoung <1104229718@qq.com>
2020-05-19 10:42:40 +0800

    READY to deploy

--- __init__.py
+++ __init__.py
@@ -7,11 +7,12 @@ bootstrap = Bootstrap()
 def create_app():
     app = Flask(__name__)
     from .watchlist import main as main_blueprint
-    config_str = "mysql+pymysql://root:123456@localhost/"
+    hostname = "localhost"
+    config_str = "mysql+pymysql://root:123456@"
     table_name = "tables"
     from watchlist.models import db
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
-    app.config['SQLALCHEMY_DATABASE_URI'] = config_str + table_name
+    app.config['SQLALCHEMY_DATABASE_URI'] = config_str + hostname + "/" + table_name
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     app.config['SQLALCHEMY_ECHO'] = True
     app.config['BOOTSTRAP_SERVE_LOCAL'] = True
